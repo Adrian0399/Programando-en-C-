364f1fd3da327897c5d8faa1963534d154215b1a
Adrian Olvera <adrianolvera03@hotmail.com>
2022-12-05 13:00:53 -0600

    First Commit

---
+++ Variables/Variables/Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Reflection;
+
+double alturaEnCm;
+alturaEnCm = 175.50;
+
+int edadAdrian = 23;
+
+Console.WriteLine(alturaEnCm);
+
+Console.WriteLine("Adrian Olvera, tiene: " + 
+    + edadAdrian + "años y mide :"
+    + alturaEnCm + " cm");
+
+//150_000_000 el "_" nos permite poder separar numeros grandes, como una ","
+int distanciaAlSol = 150_000_000;
+Console.WriteLine(distanciaAlSol);
+
+int numeroSuperGrande = int.MaxValue;
+Console.WriteLine(numeroSuperGrande);
+
+int elSignificadoDeLaVida = 42;
+
+return elSignificadoDeLaVida;
